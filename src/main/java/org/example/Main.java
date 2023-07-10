99a1dfef0371233d74b8b08d6b6b8f57c103d423
DESKTOP-DFDCC26\90506 <veliesenamzn@gmail.com>
2023-07-10 21:44:04 +0300

    add scenario steps on main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
@@ -9,5 +11,16 @@ public class Main {
         System.setProperty("webdriver.geck.driver","drivers/geckdrive");
         WebDriver webDriver=new FirefoxDriver();
         webDriver.get("https://www.amazon.com.tr/");
+
+        WebElement cookie_button = webDriver.findElement(By.id("sp-cc-accept"));
+        cookie_button.click();
+
+        WebElement search = webDriver.findElement(By.id("twotabsearchtextbox"));
+        search.sendKeys("telefon");
+
+        WebElement search_button = webDriver.findElement(By.id("nav-search-submit-button"));
+        search_button.click();
+
+
     }
 }
